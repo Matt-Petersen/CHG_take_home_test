1f146d7dedee23701466658dbe080d08dcb26db9
Matthew Petersen <PetersenMatt@outlook.com>
2024-04-10 22:03:11 -0600

    Cleaned up and added a few more asserts

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# CHG_take_home_test
+# Welcome to my take home exam
+This covers the basic testing for the Heroku app at http://the-internet.herokuapp.com/login
+
+# Funtionality covered:
+- A Test to verify the correct items are displayed on a view
+- A Test to login with invalid credentials and ensure an error is displayed
+- A test to login with valid credentials and asserts on the updated view
+
+## Running a Test
+To run the tests with Playwright installed you would use the following command
+```
+npx playwright test
+```
+If you want to run with a visual UI you would use 
+```
+npx playwright test --ui
+```
--- tests/login_page.spec.js
+++ tests/login_page.spec.js
@@ -1,16 +1,19 @@
 const { test, expect } = require('@playwright/test');
 
-test('verifies view', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('http://the-internet.herokuapp.com/login');
+});
+
+test('verifies view', async ({ page }) => {
   await expect(page).toHaveTitle(/The Internet/);
   await expect(page.getByText('Login Page')).toBeVisible();
-  await expect(page.getByTestId('username')).toBeVisible();
-  await expect(page.getByTestId('password')).toBeVisible();
-  await expect(page.getByTestId('submit')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'This is where you can log' })).toBeVisible();
+  await expect(page.getByLabel('Username')).toBeVisible();
+  await expect(page.getByLabel('Password')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
 });
 
 test('invalid login', async ({ page }) => {
-  await page.goto('http://the-internet.herokuapp.com/login');
   await page.fill('#username', 'incorrectUser');
   await page.fill('#password', 'badPassword');
   await page.click('button[type="submit"]')
@@ -18,10 +21,9 @@ test('invalid login', async ({ page }) => {
 });
 
 test('valid login', async ({ page }) => {
-  await page.goto('http://the-internet.herokuapp.com/login');
   await page.fill('#username', 'tomsmith');
-  await page.fill('#password', 'SuperSecretPassword');
+  await page.fill('#password', 'SuperSecretPassword!');
   await page.click('button[type="submit"]')
   await expect(page.getByText('You logged into a secure area!')).toBeVisible();
-  await expect(page.getByText('Secure Area')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Welcome to the Secure Area.' })).toBeVisible();
 });

31e93f5ede99c5c185d5712f025181849709618e
Matthew Petersen <PetersenMatt@outlook.com>
2024-04-10 21:05:35 -0600

    Small Clean-up

--- playwright.config.js
+++ playwright.config.js
@@ -1,25 +1,13 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 module.exports = defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -36,6 +24,11 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
     },
 
     {
@@ -64,11 +57,5 @@ module.exports = defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
 
--- tests/loginpage.spec.js
+++ tests/loginpage.spec.js
@@ -1,20 +1,10 @@
-// @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
+test('verifies page', async ({ page }) => {
   await page.goto('http://the-internet.herokuapp.com/login');
 
-  // Expect a title "to contain" a substring.
-  await expect(page.getByText('Login Page')).toBeVisible();
   await expect(page).toHaveTitle(/The Internet/);
+  await expect(page.getByText('Login Page')).toBeVisible();
+
 });
 
-// test('has login button', async ({ page }) => {
-//   await page.goto('http://the-internet.herokuapp.com/login');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
